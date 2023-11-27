b0483a1627fc70d8e8d7c2c4122df3269cc2c518
Mubíbic <90600712+mubibic@users.noreply.github.com>
2023-11-27 21:49:41 +0100

    tried to make the fetching work

---
+++ js/fetchInventoryItems.js
@@ -0,0 +1,38 @@
+const inventoryUrl = 'http://localhost:8080/inventory/items'; // Update with your actual backend URL
+
+document.addEventListener('DOMContentLoaded', getInventoryItems);
+
+async function getInventoryItems() {
+    try {
+        const response = await fetch(inventoryUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const inventoryItems = await response.json();
+        displayInventoryItems(inventoryItems);
+    } catch (error) {
+        console.error('Fetching inventory items failed:', error);
+        // Handle the error gracefully in UI
+    }
+}
+
+function displayInventoryItems(items) {
+    const table = document.querySelector('#inventoryTable tbody');
+    items.forEach(item => {
+        const tableRow = createTableRow(item);
+        table.appendChild(tableRow);
+    });
+}
+
+function createTableRow(item) {
+    const tableRow = document.createElement('tr');
+    tableRow.innerHTML = `
+        <td>${item.id}</td>
+        <td>${item.type}</td>
+        <td>${item.model}</td>
+        <td>${item.extraInformation}</td>
+        <td>${item.quantity}</td>
+        <td>${item.location}</td>
+    `;
+    return tableRow;
+}
