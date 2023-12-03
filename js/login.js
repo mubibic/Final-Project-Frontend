4eb8a69e7ebd6b7de9ebc67b7851a01b5f23fdf3
Mubíbic <90600712+mubibic@users.noreply.github.com>
2023-12-03 18:01:03 +0100

    login stuff

---
+++ js/login.js
@@ -0,0 +1,34 @@
+// Replace with your API endpoint
+const apiEndpoint = 'http://localhost:8080/login';
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+
+    fetch(apiEndpoint, {
+        mode: 'no-cors',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password })
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+        })
+        .then(data => {
+            console.log('Login successful:', data);
+            window.location.href = '../html/main-page.html';
+        })
+        .catch(error => {
+            console.error('Error during login:', error);
+            // Show error message to user
+        });
+});
