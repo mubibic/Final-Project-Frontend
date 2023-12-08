7c6e7aa684e75dbdd3de1726961f3365c4f7d0e1
BalazsGitHub <balzs.dombovai@gmail.com>
2023-12-08 16:39:49 +0100

    Authentification for log in enhanced. Now it creates a session for checking if user is logged in.

--- js/login.js
+++ js/login.js
@@ -1,33 +1,37 @@
 // Replace with your API endpoint
 const apiEndpoint = 'http://localhost:8080/login';
 
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
     event.preventDefault();
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-
-    fetch(apiEndpoint, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password })
-    })
-        .then(response => {
-            if (response.ok) {
-                return response;
-            } else {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-        })
-        .then(data => {
-            console.log('Login successful:', data);
-            window.location.href = '../html/main-page.html';
-        })
-        .catch(error => {
-            console.error('Error during login:', error);
-            // Show error message to user
+    try {
+        const response = await fetch(apiEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password })
         });
+
+        if (!response.ok) {
+            alert("Wrong username or password!");
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        // Store user data in session storage
+        if (data !== null) {
+            alert('Login successful: ' + data.username);
+            sessionStorage.setItem('HMS_isLoggedIn', 'true'); // Set session storage
+            sessionStorage.setItem('HMS_username', data.username);
+            window.location.href = '../html/main-page.html';
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        // Show error message to user
+    }
 });
--- js/navBar.js
+++ js/navBar.js
@@ -1,3 +1,29 @@
+
+// --- check if user is logged in ---
+if (sessionStorage.getItem('HMS_isLoggedIn') !== 'true') {
+    window.location.href = '../html/login.html';
+}
+
+// AUTHENTICATION
+window.onload = function() {
+    const authStatus = document.getElementById('authStatus');
+    const logoutButton = document.getElementById('logoutButton');
+
+    logoutButton.addEventListener('click', function() {
+        sessionStorage.clear();
+        location.reload();
+    });
+
+    const username = sessionStorage.getItem('HMS_username');
+    if (username) {
+        authStatus.innerHTML = 'Logged in as: [  ' + username + '  ]';
+        logoutButton.innerHTML = 'Logout';
+    }
+}
+
+
+
+
 const menuToggle = document.querySelector('.menu-toggle');
 const bxMenu = document.querySelector('.bx-menu');
 const bxX = document.querySelector('.bx-x');
