aef5ba5e781948c05c4c9709732bd23effcf48f2
BalazsGitHub <balzs.dombovai@gmail.com>
2023-12-17 18:51:45 +0100

    pdated with filtering the disposed asset page. Also done the printing function.

--- js/fetchDisposedAssetItems.js
+++ js/fetchDisposedAssetItems.js
@@ -58,3 +58,67 @@ function moveBackItem(id) {
             });
     }
 }
+
+
+document.getElementById('filterButton').addEventListener('click', function() {
+    const type = document.getElementById('filterType').value;
+    const serialNumber = document.getElementById('filterSerialNumber').value;
+    const extraInformation = document.getElementById('filterExtraInformation').value;
+    const disposalDate = document.getElementById('filterDisposalDate').value;
+    const disposalReason = document.getElementById('filterDisposalReason').value;
+    fetchFilteredItems(type, serialNumber, extraInformation, disposalDate, disposalReason);
+});
+
+function fetchFilteredItems(type, serialNumber, extraInformation, disposalDate, disposalReason) {
+    const query = new URLSearchParams({
+        type: type,
+        serialNumber: serialNumber,
+        extraInformation: extraInformation,
+        disposalDate: disposalDate,
+        disposalReason: disposalReason
+    }).toString();
+
+    fetch(`http://localhost:8080/disposed-items/items/filter?${query}`)
+        .then(response => response.json())
+        .then(items => {
+            // Clear existing items and process response as before
+            // ...
+
+            const tableBody = document.querySelector('#disposedAssetItemsTable tbody');
+            tableBody.innerHTML = '';
+
+            //showing total quantity
+            let totalQuantity = 0;
+            items.forEach(item => {
+                tableBody.appendChild(createTableRow(item));
+                totalQuantity += 1; // Increment for each item
+            });
+
+            // Update total quantity display
+            document.getElementById('totalQuantityDisplay').innerText = `Total Quantity: ${totalQuantity}`;
+        })
+        .catch(error => console.error('Error fetching filtered items:', error));
+}
+
+// ...
+
+//Print Report function
+document.getElementById('printReportButton').addEventListener('click', function() {
+    exportToExcel();
+});
+
+function exportToExcel() {
+    const itemsTable = document.querySelector('#disposedAssetItemsTable');
+    const items = Array.from(itemsTable.querySelectorAll('tbody tr')).map(row => {
+        const cells = Array.from(row.querySelectorAll('td')).slice(0, -1);
+        return cells.map(cell => cell.innerText);
+    });
+
+    const ws = XLSX.utils.aoa_to_sheet([['ID', 'Type', 'Serial Number', 'Extra Information', 'Disposal Date', 'Disposal Reason']].concat(items));
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'DisposedItemsReport');
+
+    // Save the Excel file
+    XLSX.writeFile(wb, 'DisposedItemsReport.xlsx');
+}
+// ...
